dade0eba75af953428db6ca397f9fadf3d3cabd7
LoicCK <loic.chauveau.kson@proton.me>
2025-11-27 22:29:14 +0100

    Added France Travail secrets

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI(title="JobNexus")
 
 @app.get("/")
 def read_root():
-    return {"Hello":"Welcome to JobNexus"}
+    client_id = os.environ.get("FT_CLIENT_ID", "Non défini")
+    masked_id = client_id[:4] + "*" * 10 if client_id != "Non défini" else "Non défini"
+    return {
+        "Hello": "Welcome to JobNexus",
+        "Environment": "Production" if client_id != "Non défini" else "Local",
+        "Client_ID_Check": masked_id
+    }
 
 @app.get("/health")
 def read_health():
