27d5e49e86dcf105e816bc47c6f3caa12ab209bf
LoicCK <loic.chauveau.kson@proton.me>
2025-12-02 15:11:29 +0100

    Implemented caching

--- services/cache.py
+++ services/cache.py
@@ -1,9 +1,8 @@
 from typing import List
-
 from google.cloud import firestore
 import hashlib
-
 from models.job import Job
+from datetime import datetime, timezone
 
 
 class CacheService:
@@ -16,3 +15,33 @@ class CacheService:
         return hashlib.md5(raw.encode('utf-8')).hexdigest()
 
     def save_jobs(self, query: str, lat: float, lon: float, radius: int, jobs: List[Job]):
+        research_date = datetime.now(timezone.utc)
+        cache_key = self._generate_cache_key(query, lat, lon, radius)
+        jobs_data = [job.model_dump() for job in jobs]
+        document_content = {
+            "date":research_date,
+            "params":{
+                "query":query,
+                "lat":lat,
+                "lon":lon,
+                "radius":radius
+            },
+            "jobs":jobs_data
+        }
+        self.db.collection(self.collection_name).document(cache_key).set(document_content)
+
+    def get_jobs(self, query: str, lat: float, lon: float, radius: int) -> List[Job] | None:
+        cache_key = self._generate_cache_key(query, lat, lon, radius)
+        doc_ref = self.db.collection(self.collection_name).document(cache_key)
+        doc_snapshot = doc_ref.get()
+        if not doc_snapshot.exists:
+            return None
+        data = doc_snapshot.to_dict()
+        current_date = datetime.now(timezone.utc)
+        cached_date = data["date"]
+        age = current_date - cached_date
+        if age.total_seconds() > 86400:
+            print("Expired cache")
+            return None
+        jobs_dicts = data.get("jobs", [])
+        return [Job.model_validate(j) for j in jobs_dicts]
--- services/orchestrator.py
+++ services/orchestrator.py
@@ -1,3 +1,4 @@
+from services.cache import CacheService
 from services.labonnealternance import LaBonneAlternanceService
 from services.rome import RomeService
 from services.wttj import WelcomeService
@@ -5,12 +6,20 @@ from models.job import Job
 from typing import List
 
 class OrchestratorService:
-    def __init__(self, lba_service: LaBonneAlternanceService, rome_service: RomeService, wttj_service: WelcomeService):
+    def __init__(self,
+                 lba_service: LaBonneAlternanceService,
+                 rome_service: RomeService,
+                 wttj_service: WelcomeService,
+                 cache_service: CacheService):
         self.lba_service = lba_service
         self.rome_service = rome_service
         self.wttj_service = wttj_service
+        self.cache_service = cache_service
 
     def find_jobs_by_query(self, query: str,  longitude: float, latitude: float, radius: int, insee: str) -> List[Job]:
+        cached_jobs = self.cache_service.get_jobs(query, latitude, longitude, radius)
+        if cached_jobs is not None:
+            return cached_jobs
         romes = self.rome_service.search_rome(query)
         if not romes:
             print("Aucun ROME trouvé")
