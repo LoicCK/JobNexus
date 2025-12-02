f0bf55f971a364b079706d9289353b1ee0b7c9cd
LoicCK <loic.chauveau.kson@proton.me>
2025-12-02 12:51:29 +0100

    removed base function to rewrite it

--- services/cache.py
+++ services/cache.py
@@ -16,15 +16,3 @@ class CacheService:
         return hashlib.md5(raw.encode('utf-8')).hexdigest()
 
     def save_jobs(self, query: str, lat: float, lon: float, radius: int, jobs: List[Job]):
-        search_id = self._generate_cache_key(query, lat, lon, radius)
-
-        # Sérialisation Pydantic (Job -> dict)
-        jobs_data = [job.model_dump() for job in jobs]
-
-        doc_ref = self.db.collection(self.collection_name).document(search_id)
-        doc_ref.set({
-            "query": query,
-            "jobs": jobs_data,
-            "created_at": datetime.now(timezone.utc)
-        })
-        print(f"Cache sauvegardé pour {query}")
